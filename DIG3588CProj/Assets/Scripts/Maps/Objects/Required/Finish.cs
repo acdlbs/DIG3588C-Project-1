832cf8948c3f2c4af18ea43d4d0ffdeb24e4836f
Aiden Dowell <aidendowell@ufl.edu>
2021-09-29 12:14:03 -0400

    new commit

---
+++ DIG3588CProj/Assets/Scripts/Maps/Objects/Required/Finish.cs
@@ -0,0 +1,45 @@
+﻿using Extension;
+using UnityEngine;
+
+namespace Maps.Objects.Required
+{
+    /// <summary>
+    /// Represent the finish line of a map
+    /// </summary>
+    public class Finish : SolidMapObject
+    {
+        private Animator animator;
+        private GameManager gameManager;
+
+        private bool reached;
+        
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+            gameManager = GetSceneObject<GameManager>();
+        }
+
+        protected override void OnCollision(Collider2D other, CollisionSide side)
+        {
+            if (side != CollisionSide.Top)
+            {
+                return;
+            }
+
+            if (!other.IsPlayer())
+            {
+                return;
+            }
+            
+            if (reached)
+            {
+                return;
+            }
+
+            reached = true;
+            gameManager.ChangeLevel();
+            
+            animator.SetBool("reached", reached);
+        }
+    }
+}
